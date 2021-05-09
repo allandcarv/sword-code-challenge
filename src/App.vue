99440fc171640ea37ff24a397c4ac2febcbb0d8f
Allan Carvalho <allandcarv@gmail.com>
2021-05-09 18:37:03 +0100

    Profile Section - Mobile and Desktop

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <Header />
+  <Profile />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 
+// Components
 import Header from './components/Header/header.component.vue';
+
+// Views
+import Profile from './views/Profile/profile.component.vue';
 
 @Options({
   components: {
     Header,
+    Profile,
   },
 })
 export default class App extends Vue {}
